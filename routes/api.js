083aa849e8d40b5fdc809efeabe3014781d33286
CodyHoover <codydevloop@gmail.com>
2020-07-01 11:05:11 -0700

    added html routes

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,19 @@
 const express = require ("express");
 const router = express.Router();
+const path = require("path");
 
-router.get("/", (req,res) => {
-    res.render("index");
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+    //res.send("you made it");
+})
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Develop/public/stats.html"));
+    //res.send("you made it");
+})
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Develop/public/exercise.html"));
     //res.send("you made it");
 })
 
